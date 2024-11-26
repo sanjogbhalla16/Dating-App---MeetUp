96fd8d201541f4fc483f50a240ed8dd416fc90f7
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2024-11-26 17:02:56 +0530

    Backend

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 from fastapi import FastAPI
+import config
 
 app = FastAPI()
 
--- Backend/utils/auth.py
+++ Backend/utils/auth.py
@@ -3,5 +3,37 @@ from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from typing import Union, Any
 from jose import jwt
+import config
+
+def create_access_token(subject:Union[str,Any],expires_delta:int=None)->str:
+    if expires_delta is not None:
+        expires_delta = datetime.utcnow() + expires_delta
+    else:
+        expires_delta = datetime.utcnow() + timedelta(minutes=config.ACCESS_TOKEN_EXPIRE_MINUTES)
+        
+    to_encode = {"exp":expires_delta,"sub":str(subject)}
+    encoded_jwt = jwt.encode(to_encode,config.JWT_SECRET_KEY,config.ALGORITHM)  
+    return encoded_jwt
 
 
+def create_refresh_token(subject:Union[str,Any],expires_delta:int=None)->str:
+    if expires_delta is not None:
+        expires_delta = datetime.utcnow() + expires_delta
+    else:
+        expires_delta = datetime.utcnow() + timedelta(minutes=config.REFRESH_TOKEN_EXPIRE_MINUTES)
+        
+    to_encode = {"exp":expires_delta,"sub":str(subject)}
+    encoded_jwt = jwt.encode(to_encode,config.JWT_REFRESH_SECRET_KEY,config.ALGORITHM)  
+    return encoded_jwt
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
